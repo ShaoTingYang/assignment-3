8bb7edc3ec97915f663fa55ff2b5b44aa13244b3
ShaoTingYang <141378210+ShaoTingYang@users.noreply.github.com>
2023-10-10 11:40:34 +0800

    Create EfficientTruckLoads.h

---
+++ EfficientTruckLoads.h
@@ -0,0 +1,13 @@
+#ifndef EFFICIENTTRUCKLOADS_H
+#define EFFICIENTTRUCKLOADS_H
+
+#include <unordered_map>
+
+class EfficientTruckLoads {
+public:
+    int numTrucks(int numCrates, int loadSize);
+private:
+    std::unordered_map<std::pair<int, int>, int> memo;
+};
+
+#endif
