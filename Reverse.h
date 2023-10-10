cc7f97983fe8ec4c4eacf42de6c71dc4780aefba
ShaoTingYang <141378210+ShaoTingYang@users.noreply.github.com>
2023-10-10 11:39:58 +0800

    Create Reverse.h

---
+++ Reverse.h
@@ -0,0 +1,12 @@
+#ifndef REVERSE_H
+#define REVERSE_H
+
+#include <string>
+
+class Reverse {
+public:
+    int reverseDigit(int value);
+    std::string reverseString(const std::string& letters);
+};
+
+#endif
