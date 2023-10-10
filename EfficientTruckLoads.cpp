a71521bf6ecd4f8a1a1c166f7eb46d9f50ee9e2a
ShaoTingYang <141378210+ShaoTingYang@users.noreply.github.com>
2023-10-10 11:41:09 +0800

    Create EfficientTruckLoads.cpp

---
+++ EfficientTruckLoads.cpp
@@ -0,0 +1,23 @@
+#include "EfficientTruckLoads.h"
+
+int EfficientTruckLoads::numTrucks(int numCrates, int loadSize) {
+    // Check if the result is already memoized
+    if (memo.find({numCrates, loadSize}) != memo.end()) {
+        return memo[{numCrates, loadSize}];
+    }
+    
+    // Base case: If there are fewer crates than the load size, return 1 truck
+    if (numCrates <= loadSize) {
+        memo[{numCrates, loadSize}] = 1;
+        return 1;
+    }
+    
+    // Recursive case: Split the load into two equal parts and calculate trucks needed
+    int halfLoad = numCrates / 2;
+    int trucks = numTrucks(halfLoad, loadSize) + numTrucks(numCrates - halfLoad, loadSize);
+    
+    // Memoize the result
+    memo[{numCrates, loadSize}] = trucks;
+    
+    return trucks;
+}
