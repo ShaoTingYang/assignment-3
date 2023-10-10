e5ebcce2aac3902cc4948e8a884b20f28bb71e6a
ShaoTingYang <141378210+ShaoTingYang@users.noreply.github.com>
2023-10-10 11:36:27 +0800

    Create Truckloads.cpp

---
+++ Truckloads.cpp
@@ -0,0 +1,11 @@
+#include "TruckLoads.h"
+
+int TruckLoads::numTrucks(int numCrates, int loadSize) {
+    // Base case: If there are fewer crates than the load size, return 1 truck
+    if (numCrates <= loadSize) {
+        return 1;
+    }
+    // Recursive case: Split the load into two equal parts and calculate trucks needed
+    int halfLoad = numCrates / 2;
+    return numTrucks(halfLoad, loadSize) + numTrucks(numCrates - halfLoad, loadSize);
+}
