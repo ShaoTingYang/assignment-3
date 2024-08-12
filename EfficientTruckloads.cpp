cd2b2ae7ddee154df605782d2bb7cfbf443f15e7
ShaoTingYang <141378210+ShaoTingYang@users.noreply.github.com>
2024-08-12 22:01:51 +0800

    Update EfficientTruckloads.cpp

--- EfficientTruckloads.cpp
+++ EfficientTruckloads.cpp
@@ -1,25 +1,14 @@
+
+// EfficientTruckloads.cpp
 #include "EfficientTruckloads.h"
-#include <vector>
 
-std::vector<std::vector<int>> memo; // Memoization table
-
-int EfficientTruckloads::numTruckloads(int numPackages, int maxLoad) {
-    memo.resize(numPackages + 1, std::vector<int>(maxLoad + 1, -1));
-    return calculateTruckloads(numPackages, maxLoad);
+int EfficientTruckloads::numTrucks(int numCrates, int loadSize) {
+    memo.clear(); 
+    return calculateTrucks(numCrates, loadSize);
 }
 
-int EfficientTruckloads::calculateTruckloads(int numPackages, int maxLoad) {
-    if (numPackages <= maxLoad) {
-        return 1; // Only one truck needed
-    }
-
-    if (memo[numPackages][maxLoad] != -1) {
-        return memo[numPackages][maxLoad];
-    }
-
-    int halfPackages = numPackages / 2;
-    int truckloads = calculateTruckloads(halfPackages, maxLoad) + calculateTruckloads(numPackages - halfPackages, maxLoad);
-
-    memo[numPackages][maxLoad] = truckloads;
-    return truckloads;
+int EfficientTruckloads::calculateTrucks(int numCrates, int loadSize) {
+    if (memo.find(numCrates) != memo.end()) return memo[numCrates];
+    if (numCrates <= loadSize) return memo[numCrates] = 1;
+    return memo[numCrates] = calculateTrucks((numCrates + 1) / 2, loadSize) + calculateTrucks(numCrates / 2, loadSize);
 }
