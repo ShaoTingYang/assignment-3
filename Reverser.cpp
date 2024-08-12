4fb136088593c1ac493fdbc91bc5a53bbaf2d176
ShaoTingYang <141378210+ShaoTingYang@users.noreply.github.com>
2024-08-12 21:59:15 +0800

    Update Reverser.cpp

--- Reverser.cpp
+++ Reverser.cpp
@@ -1,8 +1,20 @@
+// Reverser.cpp
+#include "Reverser.h"
+
 int Reverser::reverseDigit(int value) {
-    int reversed = 0;
-    while (value > 0) {
-        reversed = reversed * 10 + value % 10;
-        value /= 10;
-    }
+    static int reversed = 0;
+    static int remainder = 0;
+    
+    if(value <= 0) return reversed;
+    remainder = value % 10;
+    reversed = reversed * 10 + remainder;
+    reverseDigit(value / 10);
     return reversed;
+}
+
+std::string Reverser::reverseString(std::string characters) {
+    if (characters.length() <= 1) {
+        return characters;
+    }
+    return reverseString(characters.substr(1)) + characters[0];
 }
