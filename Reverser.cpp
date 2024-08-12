8641bd5877ad10f533450882c3c30564ebaa3911
ShaoTingYang <141378210+ShaoTingYang@users.noreply.github.com>
2024-08-12 21:57:08 +0800

    Update Reverser.cpp

--- Reverser.cpp
+++ Reverser.cpp
@@ -1,29 +1,8 @@
-#include "Reverser.h"
-
 int Reverser::reverseDigit(int value) {
-    if (value < 10) {
-        return value; // Single-digit number, no change needed
-    } else {
-        int lastDigit = value % 10;
-        int remainingDigits = value / 10;
-        return appendLastDigit(reverseDigit(remainingDigits), lastDigit);
+    int reversed = 0;
+    while (value > 0) {
+        reversed = reversed * 10 + value % 10;
+        value /= 10;
     }
-}
-
-std::string Reverser::reverseString(std::string characters) {
-    if (characters.empty()) {
-        return characters; // Base case: empty string
-    } else {
-        char lastChar = characters.back();
-        characters.pop_back();
-        return appendLastChar(reverseString(characters), lastChar);
-    }
-}
-
-int Reverser::appendLastDigit(int number, int digit) {
-    return number * 10 + digit;
-}
-
-std::string Reverser::appendLastChar(std::string str, char ch) {
-    return str + ch;
+    return reversed;
 }
